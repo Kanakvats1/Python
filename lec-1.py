07898a7ea325a28df4cc413809d2f9898ac4701f
Kanakvats1 <kanakvats20@gmail.com>
2024-09-26 13:11:16 +0530

    updated

---
+++ lec-1.py
@@ -0,0 +1,16 @@
+#list can store multiolke data,data can be different types int str
+#list can store the  duplicate data
+#list is an orsdered data set - sorting assending desending
+#creat list and sttore the your friends name 
+friendlist=["Ansh","kanak","vishal","prabhat","toni","nikhil","srjan"]
+#print the list of friend names
+print(friendlist)
+#add the age of your frinds into list
+#append will add the data into end of the list
+friendlist.append(26)
+friendlist.append(28)
+friendlist.append(29)
+friendlist.append(25)
+friendlist.append(23)
+friendlist.append(20)
+friendlist.append(26)
---
+++ list.py
@@ -0,0 +1,25 @@
+#list can store multiolke data,data can be different types int str
+#list can store the  duplicate data
+#list is an orsdered data set - sorting assending desending
+#creat list and sttore the your friends name 
+friendlist=["Ansh","kanak","vishal","prabhat","toni","nikhil","srjan"]
+#print the list of friend names
+print(friendlist)
+#add the age of your frinds into list
+#append will add the data into end of the list
+friendlist.append(20)
+friendlist.append(22)
+friendlist.append(21)
+friendlist.append(18)
+friendlist.append(18)
+friendlist.append(22)
+friendlist.append(20)
+print(friendlist)
+print(friendlist)
+friendlist.insert(0," rajat")
+print(friendlist)
+print(friendlist[2])
+friendlist.remove("vishal")
+print(friendlist)
+friendlist.pop(5)
+print(friendlist)
